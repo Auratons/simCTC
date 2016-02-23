a79778148b87d7ab3cf65b6d0601ee74f7e2306e
celestian <petr.celestian@gmail.com>
2016-02-23 20:50:22 +0100

    CPPCHECK: New cppcheck.sh
    We will see if it is helptfull :-)

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 report-xmlfile"
-    # debug spusteni
-    cppcheck -j3 --enable=all --platform=unix64 --std=c++11 ${PWD}/src >/dev/null
-else
-    # spusteni s generovanim XML reportu
-    cppcheck -j3 --enable=all --platform=unix64 --std=c++11 --xml ${PWD}/src 2>$1
-fi
+cppcheck \
+    --enable=all \
+    --platform=unix64 \
+    --language=c++ \
+    --std=c++11 \
+    --template=gcc \
+    --suppress=missingIncludeSystem \
+    ${PWD}/src
 
